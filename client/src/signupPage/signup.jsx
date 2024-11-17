aa03b0c8c450406bcef8b402d54970be7e666ba4
t rahul prabhu <167653990+T-Rahul-prabhu-38@users.noreply.github.com>
2024-11-17 10:44:06 +0530

    Update signup.jsx

--- client/src/signupPage/signup.jsx
+++ client/src/signupPage/signup.jsx
@@ -39,7 +39,7 @@ const LoginSignupForm = () => {
       return handleError("name, email and password are required");
     }
     try {
-      const url = `https://star-light-web-dev.vercel.app/auth/signup`;
+      const url = "https://star-light-web-dev.vercel.app/auth/signup";
       const response = await fetch(url, {
         method: "POST",
         headers: {
@@ -81,7 +81,7 @@ const LoginSignupForm = () => {
       return handleError("email and password are required");
     }
     try {
-      const url = `https://star-light-web-dev.vercel.app/auth/login`;
+      const url = "https://star-light-web-dev.vercel.app/auth/login";
       const response = await fetch(url, {
         method: "POST",
         headers: {
