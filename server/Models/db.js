bf09eaf4aef5f5122c13c3d28273df2161a61667
t rahul prabhu <167653990+T-Rahul-prabhu-38@users.noreply.github.com>
2024-11-17 10:22:39 +0530

    Update db.js

--- server/Models/db.js
+++ server/Models/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const mongo_url = "mongodb+srv://<trahulprabhu38>:<4xacZKrle97zddTW>@cluster0.8vi2o.mongodb.net/TEST?retryWrites=true&w=majority&appName=Cluster0";
+const mongo_url = "mongodb+srv://rahultest:Lonewolf@38@cluster0.8vi2o.mongodb.net/TEST?retryWrites=true&w=majority&appName=Cluster0";
 
 mongoose
   .connect(mongo_url)
